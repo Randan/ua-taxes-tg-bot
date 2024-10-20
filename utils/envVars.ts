e2bf07133fb0b1c7cfd0335899d1440a260c1e0c
Andrew Randan <andrewrandan@gmail.com>
2024-10-20 12:52:02 +0300

    Update envVars.ts

--- utils/envVars.ts
+++ utils/envVars.ts
@@ -4,5 +4,6 @@ const appPort: string = process.env.PORT || '';
 const adminId: string = process.env.ADMIN_TG_ID || '';
 const enPercent: string = process.env.EN_PERCENT || '';
 const esv: string = process.env.ESV || '';
+const militaryPercent: string = process.env.MILITARY_PERCENT || '';
 
-export { adminId, appPort, enPercent, esv };
+export { adminId, appPort, enPercent, esv, militaryPercent };
